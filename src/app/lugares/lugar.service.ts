b38e0b86d918134efcf808defc20a072dfd1d2f2
knowledgesolutionstecnology <knowledgesolutionstecnology@gmail.com>
2026-02-06 11:49:01 -0300

    cadastro de lugares

--- src/app/lugares/lugar.service.ts
+++ src/app/lugares/lugar.service.ts
@@ -1,9 +1,22 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Lugar } from './lugar';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class LugarService {
 
-  constructor() { }
+  private urlBase: string = 'http://localhost:3000'
+
+  constructor(private http: HttpClient) { }
+
+  salvar(lugar: Lugar): Observable<Lugar> {
+    return this.http.post<Lugar>(this.urlBase + '/lugares', lugar)
+  }
+
+  obterTodos(): Observable<Lugar[]> {
+    return this.http.get<Lugar[]>(this.urlBase)
+  }
 }
