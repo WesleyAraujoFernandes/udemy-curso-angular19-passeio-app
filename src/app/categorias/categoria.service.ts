5918b188247d145d0bdbc4a7b7739c5cfb5d3ee3
knowledgesolutionstecnology <knowledgesolutionstecnology@gmail.com>
2026-02-09 17:58:48 -0300

    testando environments

--- src/app/categorias/categoria.service.ts
+++ src/app/categorias/categoria.service.ts
@@ -2,21 +2,22 @@ import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Categoria } from './categoria';
 import { Observable } from 'rxjs';
+import { environment } from '../../environments/environment';
 
 @Injectable({
   providedIn: 'root'
 })
 export class CategoriaService {
 
-  private urlBase = 'http://localhost:3000'
+  apiUrl: string = environment.apiUrl + '/categorias';
   constructor(private http: HttpClient) { }
 
   salvar(categoria: Categoria): Observable<Categoria> {
-    return this.http.post<Categoria>(this.urlBase + "/categorias", categoria)
+    return this.http.post<Categoria>(this.apiUrl, categoria)
   }
 
   obterTodas(): Observable<Categoria[]> {
-    return this.http.get<Categoria[]>(this.urlBase + "/categorias");
+    return this.http.get<Categoria[]>(this.apiUrl);
   }
 }
 
